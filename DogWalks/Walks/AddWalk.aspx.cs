3629da31c0c30cd08655dac67a44197e168db03e
Benjamin Scabbia <benscabbia@gmail.com>
2015-12-11 11:10:19 +0000

    -refactored code for AddWalk page

--- DogWalks/Walks/AddWalk.aspx.cs
+++ DogWalks/Walks/AddWalk.aspx.cs
@@ -13,10 +13,7 @@ namespace DogWalks.Walks
   {
     protected void Page_Load(object sender, EventArgs e)
     {
-      if (IsPostBack)
-      {
 
-      }
     }
 
     protected void btnSave_Click(object sender, EventArgs e)
@@ -29,83 +26,86 @@ namespace DogWalks.Walks
         dogWalk.LengthID = Convert.ToInt32(LengthList.SelectedValue);
         dogWalk.Location = tbLocation.Text;
 
-        List<int> selectedTags = new List<int>();
-
-        foreach (ListItem item in cblTags.Items)
-        {
-          if (item.Selected)
-          {
-            selectedTags.Add(int.Parse(item.Value));
-          }
-        }
-
-        string tagitem = "";
-        foreach (var item in selectedTags)
-        {
-          tagitem += item + ", ";
-          dogWalk.Features.Add(db.Features.FirstOrDefault(i => i.FeatureID == item));
-
-        }
-        lbConsole.Text = tagitem;
-
-        if (FileUpload1.HasFiles)
-        {
-          foreach (var image in FileUpload1.PostedFiles)
-          {
-            Picture picture = new Picture(); //create new picture
-            
-            picture.WalkID = dogWalk.WalkID; //assign image the dogwalk id
-
-            string virtualFolder = "~/WalkPics/";
-            string physicalFolder = Server.MapPath(virtualFolder);
-            string fileName = Guid.NewGuid().ToString();
-            string extension = System.IO.Path.GetExtension(image.FileName);
-
-            image.SaveAs(System.IO.Path.Combine(physicalFolder, fileName + extension));  //save image on local
-
-            picture.PictureUrl = virtualFolder + fileName + extension; //set picture url
-
-            picture.Description = "an image of the walk"; //set description (temp)
-            
-            dogWalk.Pictures.Add(picture); //add the image to pictures
-          }
-
-        }
- 
-        //http://stackoverflow.com/questions/1196007/linq-get-all-selected-values-of-a-checkboxlist-using-a-lambda-expression
-        //IEnumerable<int> allChecked = chkBoxList.Items
-        //                              .Cast<ListItem>()
-        //                              .Where(item => item.Selected)
-        //                              .Select(item => int.Parse(item.Value));
-
-
-        //List<ListItem> selectedTags = cblTags.Items.Cast<ListItem>()
-        //  .Where(li => li.Selected)
-        //  .Select(li => li.Value).ToList();
-
-        //convert any postcode to no-space, lowercase format
-        string postcode = tbPostcode.Text.ToLower().Replace(" ", "");
-        //get postcode coordinates
-        var postCodeCoords = (from p in db.PostCodesUKs
-                              where p.PostcodeNoSpace == postcode
-                              select p).SingleOrDefault();
-
-        decimal postcodeLat = postCodeCoords.Latitude;
-        decimal postcodeLong = postCodeCoords.Longitude;
-
-        dogWalk.Postcode = postCodeCoords.Postcode; //correctly formatted postcode
-        dogWalk.Latitude = postcodeLat;
-        dogWalk.Longitude = postcodeLong;
+        TagLogic(db, dogWalk);
+        ImageLogic(dogWalk);               
+        PostCodeLogic(db, dogWalk);
 
         dogWalk.CreateDateTime = DateTime.Now;
         dogWalk.UpdateDateTime = dogWalk.CreateDateTime;
-
 
         db.DogWalks.Add(dogWalk);
         db.SaveChanges();
         Response.Redirect("~/Walks/ListWalks.aspx");
       }
-    }   
+    }
+
+    //method which contains image logic
+    private void ImageLogic(DogWalk dogWalk)
+    {
+      if (FileUpload1.HasFiles)
+      {
+        foreach (var image in FileUpload1.PostedFiles)
+        {
+          Picture picture = new Picture(); //create new picture
+
+          picture.WalkID = dogWalk.WalkID; //assign image the dogwalk id
+
+          string virtualFolder = "~/WalkPics/";
+          string physicalFolder = Server.MapPath(virtualFolder);
+          string fileName = Guid.NewGuid().ToString();
+          string extension = System.IO.Path.GetExtension(image.FileName);
+
+          image.SaveAs(System.IO.Path.Combine(physicalFolder, fileName + extension));  //save image on local
+
+          picture.PictureUrl = virtualFolder + fileName + extension; //set picture url
+
+          picture.Description = "an image of the walk"; //set description (temp)
+
+          dogWalk.Pictures.Add(picture); //add the image to pictures
+        }
+
+      }
+    }
+
+    //method which contains tag logic
+    private void TagLogic(WalkContext db, DogWalk dogWalk)
+    {
+      List<int> selectedTags = new List<int>();
+      foreach (ListItem item in cblTags.Items)
+      {
+        if (item.Selected)
+        {
+          selectedTags.Add(int.Parse(item.Value));
+        }
+      }
+
+      string tagitem = "";
+      foreach (var item in selectedTags)
+      {
+        tagitem += item + ", ";
+        dogWalk.Features.Add(db.Features.FirstOrDefault(i => i.FeatureID == item));
+
+      }
+      lbConsole.Text = tagitem;
+    }
+
+    //method which contains postcode logic
+    private void PostCodeLogic(WalkContext db, DogWalk dogWalk)
+    {
+      //convert any postcode to no-space, lowercase format
+      string postcode = tbPostcode.Text.ToLower().Replace(" ", "");
+      //get postcode coordinates
+      var postCodeCoords = (from p in db.PostCodesUKs
+                            where p.PostcodeNoSpace == postcode
+                            select p).SingleOrDefault();
+
+      decimal postcodeLat = postCodeCoords.Latitude;
+      decimal postcodeLong = postCodeCoords.Longitude;
+
+      dogWalk.Postcode = postCodeCoords.Postcode; //correctly formatted postcode
+      dogWalk.Latitude = postcodeLat;
+      dogWalk.Longitude = postcodeLong;
+    }
 
     public IEnumerable<Feature> TagList_GetData()
     {
@@ -157,3 +157,14 @@ namespace DogWalks.Walks
 //have to reduce overhead from application. 
 
 //IQueryable prepares high performance in such situations (huge data) by filtering data firstly and then sending filtered data to client.
+
+//http://stackoverflow.com/questions/1196007/linq-get-all-selected-values-of-a-checkboxlist-using-a-lambda-expression
+//IEnumerable<int> allChecked = chkBoxList.Items
+//                              .Cast<ListItem>()
+//                              .Where(item => item.Selected)
+//                              .Select(item => int.Parse(item.Value));
+
+
+//List<ListItem> selectedTags = cblTags.Items.Cast<ListItem>()
+//  .Where(li => li.Selected)
+//  .Select(li => li.Value).ToList();-
